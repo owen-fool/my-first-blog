5debf5ba1f584cc5c0a4610ecf065f2385d22ac4
owen-fool <owenrmilner@gmail.com>
2020-05-28 13:30:17 +0100

    Functional test for clicking CV button begun

--- functional_tests.py
+++ functional_tests.py
@@ -5,14 +5,18 @@ class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+        self.browser.get('http://localhost:8000')
 
     def tearDown(self):
         self.browser.quit()
 
     def test_is_it_blog(self):
-        self.browser.get('http://localhost:8000')
+       
         self.assertIn('Blog', self.browser.title)
         #self.fail('Finish the test!')
+
+    def test_click_to_CV(self):
+        self.browser.find_element_by_xpath("/html/body/div[1]/a[1]/span").click()
 
 if __name__ == '__main__':
     unittest.main()
